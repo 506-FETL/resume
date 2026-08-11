16591c9302f4da8b1e5f5fb990cc3506bf78e252
shemingcong.123 <shemingcong.123@bytedance.com>
2026-08-11 23:35:16 +0800

    perf(resume): 仅打包文楷 WOFF2 字体（任务 8/9）

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import App from './App'
 import ErrorFallback from './components/ErrorBoundary'
 import '@fontsource-variable/noto-sans-sc/wght.css'
 import '@fontsource-variable/noto-serif-sc/wght.css'
-import '@fontsource/lxgw-wenkai/500.css'
-import '@fontsource/lxgw-wenkai/700.css'
 import './index.css'
 
 const app = createRoot(document.getElementById('root')!)
